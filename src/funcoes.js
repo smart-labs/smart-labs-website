4b85a74634c44de2c29586c3116424aadbbdeba4
Adriano Araújo <adrianoalvescru@gmail.com>
2019-09-13 15:13:56 -0300

    Estrura básica site, new version

--- src/funcoes.js
+++ src/funcoes.js
@@ -1,3 +1,35 @@
+var intervalo = 2000;
+
 function mudaFoto(foto) {
   document.getElementById("icone").src = foto;
+}
+
+function slide1() {
+  document.getElementById("banner").src = "fotos/logo1.jpg";
+  setTimeout("slide2()", intervalo);
+}
+
+function slide2() {
+  document.getElementById("banner").src = "fotos/cesar.png";
+  setTimeout("slide3()", intervalo);
+}
+
+function slide3() {
+  document.getElementById("banner").src = "fotos/difusora.png";
+  setTimeout("slide4()", intervalo);
+}
+
+function slide4() {
+  document.getElementById("banner").src = "fotos/prefeitura.png";
+  setTimeout("slide5()", intervalo);
+}
+
+function slide5() {
+  document.getElementById("banner").src = "fotos/porto.png";
+  setTimeout("slide6()", intervalo);
+}
+
+function slide6() {
+  document.getElementById("banner").src = "fotos/armazem.png";
+  setTimeout("slide1()", intervalo);
 }
